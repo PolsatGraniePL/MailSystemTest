88dc3c039b5885d4790037d45e6789577b6fc60c
Polsat GraniePL <k.ksiazek.yt@gmail.com>
2022-03-05 22:04:57 +0100

    Dodaj pliki projektów.

---
+++ MailSystemTest/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace MailSystemTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(226, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(226, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(76, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1087, 494);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lmao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MailSystemTest/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailSystemTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int y = 50;
+        int ilosc = 0;
+        List<Panel> panels = new List<Panel>();
+        List<Label> teksty = new List<Label>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewPanelCreator();
+
+
+            //teksty[5].Text = teksty[4].Text;
+            //teksty[4].Text = teksty[3].Text;
+            //teksty[3].Text = teksty[2].Text;
+            //teksty[2].Text = teksty[1].Text;
+
+            //teksty[1].Text = teksty[4].Text;
+
+            //label5.Text = label4.Text;
+            //label4.Text = label3.Text;
+            //label3.Text = label2.Text;
+            //label2.Text = label1.Text;
+            //label1.Text = textBox1.Text;
+
+
+        }
+
+        private void NewPanelCreator()
+        {
+            var newPanel = new Panel();
+            newPanel.BackColor = Color.Gray;
+            newPanel.Location = new Point(400, y);
+            newPanel.Size = new Size(400, 40);
+            newPanel.Name = "panelX" + ilosc.ToString();
+            Controls.Add(newPanel);
+
+            panels.Add(newPanel);
+            y += 50;
+            ilosc++;
+
+            var labelNew = new Label();
+            labelNew.Location = new Point(0, 0);
+            labelNew.Size = new Size(667, 15);
+            labelNew.Text = textBox1.Text;
+            newPanel.Controls.Add(labelNew);
+            teksty.Add(labelNew);
+
+            label1.Text = "";
+            int index = 0;
+            foreach (var test in teksty)
+            {
+                if (teksty.Count == index + 1)
+                {
+                    return;
+                }
+                label1.Text += "\n(" + teksty.Count + " | " + index + ") " + test.Text;
+                teksty[index].Text = teksty[index + 1].Text;
+                index++;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            panels[4].BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            int index = 0;
+            foreach (var test in teksty)
+            {
+                if (teksty.Count == index + 1)
+                {
+                    return;
+                }
+                label1.Text += "\n(" + teksty.Count + " | " + index + ") " + test.Text;
+                teksty[index].Text = teksty[index+1].Text;
+                index++;
+            }
+        }
+    }
+}
